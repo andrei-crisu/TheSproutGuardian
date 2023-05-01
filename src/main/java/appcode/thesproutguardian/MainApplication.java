b979eb3ebce4b53a123e935dd4d5029bded76dcd
Andrei Crișu <crisuraduandrei@gmail.com>
2023-05-01 09:42:28 +0300

    implemented on close ask window

--- src/main/java/appcode/thesproutguardian/MainApplication.java
+++ src/main/java/appcode/thesproutguardian/MainApplication.java
@@ -5,16 +5,20 @@ import appcode.thesproutguardian.utile.SproutParam;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Optional;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class MainApplication extends Application {
     SproutParam sproutParameteres;
     MainController mainController;
+    Timer timer;
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("mainView.fxml"));
@@ -32,7 +36,7 @@ public class MainApplication extends Application {
         stage.setScene(scene);
         mainController = fxmlLoader.getController();
 
-        Timer timer = new Timer();
+        timer = new Timer();
         TimerTask task = new SensorData(mainController.humidityLabel,
                 mainController.temperatureLabel,
                 mainController.luminosityLabel,
@@ -45,6 +49,25 @@ public class MainApplication extends Application {
         mainController.temperatureLabel.setText(String.valueOf(sproutParameteres.getTemperature())+"");
         mainController.luminosityLabel.setText(String.valueOf(sproutParameteres.getLuminosity())+"%");
         mainController.moistureLabel.setText(String.valueOf(sproutParameteres.getMoisture())+"%");
+
+
+        stage.setOnCloseRequest(event -> {
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Sprout :: EXIT ");
+            alert.setHeaderText("Are you sure you want to exit?");
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.isPresent() && result.get() == ButtonType.OK) {
+                // Stop the timer task
+                timer.cancel();
+                // Close the application
+                stage.close();
+            } else {
+                // Consume the event to prevent the application from closing
+                event.consume();
+            }
+        });
+
+
         stage.show();
 
 
